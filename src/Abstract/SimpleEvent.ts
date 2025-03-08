21772c9833da57c3a64ae1d5e27fa558a8f9351f
Austin Hummel <austin.hummel@sentience.llc>
2025-03-08 13:58:25 -0500

    Clean up and document SimpleEvent implementation

--- src/Abstract/SimpleEvent.ts
+++ src/Abstract/SimpleEvent.ts
@@ -1,36 +1,42 @@
-export class SimpleEvent<TEventArgs extends any[]> extends EventTarget implements IEvent<TEventArgs> {
-    /** Subscribe to the event. */
+/** A simple event that can be subscribed to. (Browser-Only: backed by EventTarget and CustomEvent) */
+export class SimpleEvent<TEventArgs extends any[]> implements IEvent<TEventArgs> {
+    private readonly target = new EventTarget();
+
+    /** Create a new instance of SimpleEvent.
+     * @param eventName The name of the event. Default is 'SimpleEvent'.
+     */
+    constructor(private readonly eventName: string = 'SimpleEvent') { }
+
+    /** Subscribe to the event.
+     * @param callback The callback to invoke when the event is raised.
+     * @returns An object that can be used to unsubscribe from the event.
+    */
     public subscribe(callback: (...args: TEventArgs) => void): IEventSubscription {
-        // Define an event handler that extracts the detail from our custom event.
         const handler = (e: Event) => {
-            if (e instanceof CustomEvent) {
-                callback(...e.detail);
-            }
+            const ce = e as CustomEvent;
+            callback(...ce.detail);
         };
-        // Listen to the custom event type (you can change 'custom' if needed).
-        this.addEventListener('custom', handler as EventListener);
-        // Return an unsubscribable
-        return { unsubscribe: () => this.removeEventListener('custom', handler as EventListener) };
+        
+        this.target.addEventListener(this.eventName, handler);
+
+        return {
+            unsubscribe: () => this.target.removeEventListener(this.eventName, handler)
+        };
     }
 
-    /** Invoke the event with data. */
-    public get invoke(): (...args: TEventArgs) => void {
-        return this.callSubscribers.bind(this) as any;
-    }
-
-    // Raise the event with data.
-    private callSubscribers(...args: any[]): void {
-        // Create a new CustomEvent carrying our data.
-        const event = new CustomEvent('custom', { detail: args });
-        // Dispatch the event to all subscribers.
-        this.dispatchEvent(event);
+    /** Invoke the event. */
+    public invoke(...args: TEventArgs): void {
+        const event = new CustomEvent(this.eventName, { detail: args });
+        this.target.dispatchEvent(event);
     }
 }
 
+/** An event that can be subscribed to. */
 export interface IEvent<TEventArgs extends any[]> {
     subscribe(callback: (...args: TEventArgs) => void): IEventSubscription;    
 }
 
+/** An object that can be used to unsubscribe from an event. */
 export interface IEventSubscription {
     unsubscribe(): void;
 }
